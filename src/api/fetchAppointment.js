cd48553f5e9594a2613a506d1ce20a26bc71f963
JoaquinxHerrera <joaquin.xherrera1@gmail.com>
2024-09-14 09:15:10 -0300

    proxy

--- src/api/fetchAppointment.js
+++ src/api/fetchAppointment.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const BACKEND_ENDPOINT = import.meta.env.VITE_BACKEND_URL;
+// const BACKEND_ENDPOINT = import.meta.env.VITE_BACKEND_URL;
 
 export const fetchAppointments = async (idPaciente, token) => {
     try {
-        const response = await axios.get(`${BACKEND_ENDPOINT}/consultas/${idPaciente}`,{
+        const response = await axios.get(`/api/consultas/${idPaciente}`,{
             headers: {
                 Authorization: `Bearer ${token}`,
             },
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,26 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Esto redirige las solicitudes hechas a /api al servidor backend
+      '/api': {
+        target: 'https://saludvital-production.up.railway.app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // Elimina el prefijo /api al hacer la solicitud al backend
+      }
+    }
+  }
 })
-
-
