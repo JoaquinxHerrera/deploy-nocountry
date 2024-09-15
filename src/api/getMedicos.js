f2f0bb912e3b9f7bf898bcecfb6c60474137c45d
JoaquinxHerrera <joaquin.xherrera1@gmail.com>
2024-09-15 12:50:56 -0300

    no cors

--- src/api/getMedicos.js
+++ src/api/getMedicos.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// const BACKEND_ENDPOINT = import.meta.env.VITE_BACKEND_URL
+const BACKEND_ENDPOINT = import.meta.env.VITE_BACKEND_URL
 
 export const getMedicos = async () => {
     try {
@@ -9,7 +9,7 @@ export const getMedicos = async () => {
         let totalPages = 1;
         
         while (page < totalPages) {
-            const response = await axios.get(`/api/medicos/medicos?page=${page}`);
+            const response = await axios.get(`${BACKEND_ENDPOINT}/medicos/medicos?page=${page}`);
 
             if (response.data && response.data.content && response.data.totalPages !== undefined) {
                 const { content, totalPages: pages } = response.data;
--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,9 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://backend-sin-auth-production.up.railway.app',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
-});
+})
+
+
